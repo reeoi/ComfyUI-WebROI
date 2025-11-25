3f445f8f7b157f72d69ee22f594b09c69cb2a4f9
reeoi <54705406+reeoi@users.noreply.github.com>
2025-11-25 16:43:38 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .web_roi import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-# 这一行告诉 ComfyUI：前端文件在当前目录下的 js 文件夹里
-WEB_DIRECTORY = "./js"
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
